f7a5c866a55244827143a805b42bcffb3fcb497b
tangemmy <emmtang@ucdavis.edu>
2024-03-19 14:19:32 -0700

    final 2

--- finalmadlibs/script.js
+++ finalmadlibs/script.js
@@ -1,39 +1,72 @@
-(function(){
+(function() {
     "use strict";
     console.log("reading js");
     
+    // Select elements
     const home = document.querySelector("#home");
-    const desk = document.querySelector("#formdiv")
-    //clickable areas outside
+    const desk = document.querySelector("#formdiv");
+    
+    // Initialize audio objects
+    const bgm = new Audio("sounds/cloud9.mp3");
+    bgm.loop = true;
+    const doorbell = new Audio("sounds/opendoor.mp3");
+    const bush = new Audio("sounds/catbush.mp3");
+    const treecat = new Audio("sounds/treemeow.mp3");
+    
+    // Select clickable areas
     const door = document.querySelector("#door");
     const pot = document.querySelector("#pot");
     const tree = document.querySelector("#tree");
-
-    //hover effect for home items
-    door.addEventListener("mouseover",function(){
-        home.src="images/doorglow.png";
+    const back = document.querySelector("#back");
+    // Play background music on mouse move
+    window.addEventListener("mousemove", function() {
+        bgm.play();
     });
-    pot.addEventListener("mouseover",function(){
-        home.src="images/potglow.png";
+    
+    // Hover effects
+    door.addEventListener("mouseover", function() {
+        home.src = "images/doorglow.png";
     });
-    tree.addEventListener("mouseover",function(){
-        home.src="images/treeglow.png";
+    pot.addEventListener("mouseover", function() {
+        home.src = "images/potglow.png";
     });
-    //remove hover effects on mouse out
-    door.addEventListener("mouseout", function(){
+    tree.addEventListener("mouseover", function() {
+        home.src = "images/treeglow.png";
+    });
+    
+    // Remove hover effects on mouse out
+    door.addEventListener("mouseout", function() {
         home.src = "images/home.PNG";
     });
-    pot.addEventListener("mouseout", function(){
+    pot.addEventListener("mouseout", function() {
         home.src = "images/home.PNG";
     });
-    tree.addEventListener("mouseout", function(){
+    tree.addEventListener("mouseout", function() {
         home.src = "images/home.PNG";
     });
-
-    //Clicking items
-    door.addEventListener("click",function(event){
+    
+    // Clickable areas actions
+    door.addEventListener("click", function(event) {
         event.preventDefault();
-        desk.className="showing";
-        home.className="hidden";
+        desk.className = "showing";
+        home.className = "hidden";
+        back.className="showing";
+        doorbell.play();
     });
+    pot.addEventListener("click", function(event) {
+        event.preventDefault();
+        home.src = "images/potcat.png";
+        bush.play();
+    });
+    tree.addEventListener("click", function(event) {
+        event.preventDefault();
+        home.src = "images/treetail.png";
+        treecat.play();
+    });
+    back.addEventListener("click",function(event){
+        desk.className="hidden";
+        home.className="showing";
+        back.className="hidden";
+    });
+
 })();
